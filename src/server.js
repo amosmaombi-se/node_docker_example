ee742b0802abe2915860c828d0afc2f3c0726889
amosmaombi <maombibetets@gmail.com>
2024-03-01 14:17:08 +0300

    adding second url

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ app.get('/', (req,res) => {
     res.send("Welcome to my awesome app with ci/cd tests")
 });
 
+app.get('/about', (req, res) => {
+    res.status(200).json({
+      message: 'About Dummy Server',
+      description: 'This is a simple Express.js server for demonstration purposes.',
+      version: '1.0'
+    });
+  });
+  
 app.listen(3000, function(){
      console.log("App listen to port 3000")
 });
